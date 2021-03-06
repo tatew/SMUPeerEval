4e1d10468155424d186d452dd88e2e3e9cbe308e
marcusstaylor <marcus99@vt.edu>
2021-03-06 13:12:37 -0500

    Added professor and course tables

--- evals/models.py
+++ evals/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Professor(models.Model):
+    lastName = models.CharField(max_length=50)
+    firstName = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    school = models.CharField(max_length=100)
+
+class Course(models.Model):
+    courseName = models.CharField(max_length=50)
+    courseNumber = models.IntegerField()
+    maxStudents = models.IntegerField()
+    meetingTime = models.DateTimeField('meeting time')
+    discipline = models.CharField(max_length=50)
+    professor = models.ForeignKey(Professor, on_delete=models.RESTRICT)
