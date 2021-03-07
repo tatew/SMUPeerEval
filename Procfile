4c1e2fd5385d4dd0a9436c8722b5b8784e53f991
Tate <tatew@vt.edu>
2021-03-07 00:37:41 -0500

    Setup for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn SMUPeerEval.wsgi --log-file -
--- SMUPeerEval/settings.py
+++ SMUPeerEval/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,4 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+django_heroku.settings(locals())
